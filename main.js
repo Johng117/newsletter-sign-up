18d59e47877616e8918be4aa4fe02161f97a86df
Johng117 <johngorman117@gmail.com>
2025-02-26 19:40:47 +0000

    updates to logic in main.js and other files

--- main.js
+++ main.js
@@ -1,4 +1,30 @@
+let errorStateColor = "hsl(4, 100%, 67%)";
+let input = document.getElementById("email");
+let btn = document.getElementsByTagName("button")[0];
+let errorText = document.getElementsByClassName("email-error-message");
+let textValue = "";
+let re = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/
 
-// if(document)
-let headerText = document.getElementsByTagName("h1");
-console.log(headerText[0].innerText);
+function handleInput(e) {
+  e.preventDefault();
+  textValue = e.target.value;
+
+  // removes error state and styles on change of input
+  errorText[0].style.display = "none";
+  input.style.color = "hsl(234, 29%, 20%)";
+  input.style.border = `1px solid hsl(0, 0%, 58%)`;
+  input.style.background = "hsl(0, 0%, 100%)";
+}
+
+function handleButtonClick(e) {
+  e.preventDefault();
+  if (textValue.length < 1 || re.test(textValue)===false) {
+    errorText[0].style.display = "block";
+    errorText[0].style.color = errorStateColor;
+    input.style.color = errorStateColor;
+    input.style.border = `3px solid ${errorStateColor}`;
+    input.style.background = "hsl(4, 100%, 95%)";
+  }
+}
+input.addEventListener("input", handleInput);
+btn.addEventListener("click", handleButtonClick);
