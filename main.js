2029ed159256f3b3eae2aa38135a6638077b15f1
Johng117 <johngorman117@gmail.com>
2025-06-15 09:28:54 +0100

    corrections to desktop success css and adding of keydown event

--- main.js
+++ main.js
@@ -10,6 +10,7 @@ const input = document.getElementById("email");
 const btn = document.getElementsByTagName("button")[0];
 const errorText = document.getElementsByClassName("email-error-message")[0];
 const dismissBtn = document.getElementsByClassName("dismiss")[0];
+let mainEl = document.querySelector("main");
 const emailRegex =
   /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/;
 
@@ -27,7 +28,7 @@ function handleEmailSubmit(e) {
   const formData = new FormData(e.target);
   const data = Object.fromEntries(formData);
   console.log(data);
-  
+
   if (data.email.length < 1 || emailRegex.test(data.email) === false) {
     errorText.style.display = "block";
     errorText.style.color = errorStateColor;
@@ -36,6 +37,7 @@ function handleEmailSubmit(e) {
     input.style.background = "hsl(4, 100%, 95%)";
   } else {
     let successEl = document.getElementsByClassName("success")[0];
+    mainEl.style.backgroundColor = "hsl(234, 29%, 20%)";
     for (element of sectionEls) {
       element.style.display = "none";
     }
@@ -48,6 +50,7 @@ function handleDismiss(e) {
   e.preventDefault();
   let dismissEl = document.getElementsByClassName("success")[0];
   dismissEl.style.display = "none";
+  mainEl.style.backgroundColor = "hsl(0, 0%, 100%)";
   for (let i = 0; i <= 3; i++) {
     sectionEls[i].style.display = "block";
   }
